a6fd56079c4224de7ba7fea73694664aa09efea4
afbastos <andrebastos2601@gmail.com>
2022-10-23 23:43:00 +0100

    Added first auth triggers

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,7 @@ import { applicationDefault } from "firebase-admin/app";
 import * as firebase_functions from "firebase-functions";
 const firebase_admin = require("firebase-admin");
 
+
 // Add this in firebase.json, predeploy on top of the build line - to enable linter
 // "npm --prefix \"$RESOURCE_DIR\" run lint",
 
@@ -18,13 +19,13 @@ const admin_db = firebase_admin.firestore();
 // https://firebase.google.com/docs/functions/typescript
 
 export const helloWorld = firebase_functions.region('europe-west1').https.onRequest((request, response) => {
-    firebase_functions.logger.info("Hello logs!", {structuredData: true});
+    firebase_functions.logger.log("Hello logs!", {structuredData: true});
     response.send("Hello from Firebase!");
 });
 
 export const match_create = firebase_functions.region('europe-west1').https.onRequest(async (request, response) => {
     try{
-        firebase_functions.logger.info("Match creation initiated");
+        firebase_functions.logger.log("Match creation initiated");
         const matchesDb = admin_db.collection('matches');
         // generate id TODO
         let id = 1
@@ -33,7 +34,7 @@ export const match_create = firebase_functions.region('europe-west1').https.onRe
         }
         let match = await matchesDb.doc(id).set(gameJson);
 
-        firebase_functions.logger.info("Match created", {structuredData: true});
+        firebase_functions.logger.log("Match created", {structuredData: true});
         response.send(match);
     } catch (error){
         firebase_functions.logger.error(error)
@@ -41,7 +42,19 @@ export const match_create = firebase_functions.region('europe-west1').https.onRe
 });
 
 
+// AUTH
+// Create
+export const newUserSignup = firebase_functions.region('europe-west1').auth.user().onCreate(user => {
+    // firebase_functions.logger.log("User created", {email: user.email, uid:user.uid});
+    return admin_db.collection('users').doc(user.uid).set({
+        email: user.email
+    })
+})
+// Delete
+export const userDeleted = firebase_functions.region('europe-west1').auth.user().onDelete(user => {
+    // firebase_functions.logger.log("User deleted", {email: user.email, uid:user.uid});
+    const to_delete = admin_db.collection('users').doc(user.uid);
+    return to_delete.delete();
+})
 
-
-        
 
